778166b349cde6b4b10beeda046f425e64f285ae
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-11-30 23:14:20 -0500

    working on server and client

--- among-us-data-viewer/src/App.js
+++ among-us-data-viewer/src/App.js
@@ -7,26 +7,23 @@ import { Navbar } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let rows = [];
+let cols = [];
+
+fetch('http://localhost:4000/data')
+    .then(res => res.json())
+    .then((data) => {
+        cols = data.cols
+        rows = data.rows;
+    })
+    .catch(console.log)
 
 const App = () => {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
 
-    const [rowData, setRowData] = useState([
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000},
-        {make: "Porsche", model: "Boxter", price: 72000}
-    ]);
+    const [colDefs, setColDefs] = useState(cols);
+    const [rowData, setRowData] = useState(rows);
 
     return (
         <>
@@ -50,9 +47,10 @@ const App = () => {
                         }
                     }
                     rowData={rowData}>
-                    <AgGridColumn field="make" sortable={true} filter={true}></AgGridColumn>
+                    {colDefs.map(def => <AgGridColumn field="def" sortable={true} filter={true} />)}
+                    {/* <AgGridColumn field="make" sortable={true} filter={true}></AgGridColumn>
                     <AgGridColumn field="model" sortable={true} filter={true}></AgGridColumn>
-                    <AgGridColumn field="price" sortable={true} filter={true}></AgGridColumn>
+                    <AgGridColumn field="price" sortable={true} filter={true}></AgGridColumn> */}
                 </AgGridReact>
             </div>
         </>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
-app.get('/test', (req, res) => {
-  res.send('Hello World!')
+app.get('/data', (req, res) => {
+    res.json({
+        cols: ["make", "model", "price"],
+        rows: [
+            {make: "Toyota", model: "Celica", price: 35000},
+            {make: "Ford", model: "Mondeo", price: 32000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000},
+            {make: "Porsche", model: "Boxter", price: 72000}
+        ]
+    });
 })
 
 app.use(express.static('../build'))
 
 app.listen(port, () => {
-  console.log(`Serving site to http://localhost:${port}`)
+    console.log(`Serving site to http://localhost:${port}`)
 })
